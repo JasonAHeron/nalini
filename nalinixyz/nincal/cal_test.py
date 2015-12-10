b1e3ba308049456378b40b7e03cc0c718bfb4229
Jason Heron <jheron@disqus.com>
2015-12-09 17:40:00 -0800

    changing name

---
+++ nalinixyz/nincal/cal_test.py
@@ -0,0 +1,5 @@
+import calendar
+
+month = calendar.monthcalendar(2015, 12)
+
+print(month)
--- nalinixyz/nincal/views.py
+++ nalinixyz/nincal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import calendar
 
 def index(request):
     return render(request, 'cal.html')
