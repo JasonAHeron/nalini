78ef9eec4d28472fc3abcb68a4350e9779697996
Jason Heron <jheron@disqus.com>
2015-12-09 21:05:07 -0800

    updating date logic

--- nalinixyz/nincal/cal_test.py
+++ nalinixyz/nincal/cal_test.py
@@ -1,5 +1,15 @@
 import calendar
+import datetime
+import re
 
-month = calendar.monthcalendar(2015, 12)
 
-print(month)
+YEAR = ['January',  'February',  'March',  'April',  'May',  'June',  'July',  'August',  'September',  'October',  'November',  'December']
+
+
+today = datetime.datetime.date(datetime.datetime.now())
+
+month = calendar.monthcalendar(today.year, today.month)
+
+
+
+
--- nalinixyz/nincal/views.py
+++ nalinixyz/nincal/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 import calendar
+import datetime
+
+
+YEAR = ['January',  'February',  'March',  'April',  'May',  'June',  'July',  'August',  'September',  'October',  'November',  'December']
+
 
 def index(request):
-    return render(request, 'cal.html')
+    today = datetime.datetime.date(datetime.datetime.now())
+    time_dict = {
+        'month_number': today.month,
+        'day': today.day,
+        'month_name' : YEAR[today.month-1],
+        'weeks' : calendar.monthcalendar(today.year, today.month)
+    }
+    return render(request, 'cal.html', time_dict)
