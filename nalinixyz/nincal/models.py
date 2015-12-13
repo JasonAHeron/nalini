c67e2c6f493ea811eeaefcdb74f6006e5e473f22
Jason Heron <jheron@disqus.com>
2015-12-12 22:09:44 -0800

    slight refactor and new day

--- nalinixyz/nincal/models.py
+++ nalinixyz/nincal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Day(models.Model):
+    date = models.DateField()
+
+
+class Event(models.Model):
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    content = models.CharField()
--- nalinixyz/nincal/views.py
+++ nalinixyz/nincal/views.py
@@ -2,14 +2,13 @@ import datetime
 from pytz import timezone
 from django.shortcuts import render
 from nincal.helpers.constants import YEAR
-from nincal.helpers.calendar_helper import create_month
+from nincal.helpers.calendar_helper import create_month, get_times
 from nincal.helpers.forismatic_api import get_quote
 
 def index(request):
     today_in_india = datetime.datetime.now(timezone('Asia/Calcutta'))
     today = today_in_india.date()
     quote, author = get_quote()
-    #month = create_month(today)
 
     data = {
         'month_number': today.month,
@@ -25,21 +24,17 @@ def index(request):
 
 def days(request):
     override = request.GET.get('override')
-    day_iso = datetime.datetime(*map(int, request.GET.get('day').split('-'))).date().isoformat()
-    now_in_india = datetime.datetime.now(timezone('Asia/Calcutta'))
-    now_in_america = datetime.datetime.now(timezone('America/Los_Angeles'))
-    now_iso = now_in_india.date().isoformat()
-    data = {'day_clicked': day_iso,
-            'now_iso': now_iso,
-            'time_in_india': now_in_india,
-            'time_in_america': now_in_america,
-            }
+
+    time_data = get_times(request)
 
     if override:
-        return render(request, '{}.html'.format(override), data)
-    if day_iso <= '2015-12-11':
-        return render(request, 'withme.html', data)
-    if day_iso <= now_iso:
-        return render(request, '{}.html'.format(day_iso), data)
+        return render(request, '{}.html'.format(override), time_data)
+
+    if time_data['day_iso'] <= '2015-12-11':
+        return render(request, 'withme.html', time_data)
+
+    if time_data['day_iso'] <= time_data['now_iso']:
+        return render(request, '{}.html'.format(time_data['day_iso']), time_data)
+
     else:
-        return render(request, 'day_unavailable.html', data)
+        return render(request, 'day_unavailable.html', time_data)
