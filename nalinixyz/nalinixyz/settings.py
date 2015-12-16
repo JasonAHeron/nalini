aeec0de4883e0f9173ddf6ee8391533beacc3702
Jason Heron <jheron@disqus.com>
2015-12-16 15:09:12 -0800

    Revert "begin massive refactor"
    This reverts commit 6ff925285437f62ada266927a5ff4ba3beddc959.

--- nalinixyz/nalinixyz/settings.py
+++ nalinixyz/nalinixyz/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'days.apps.DaysConfig',
     'nincal.apps.NincalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- nalinixyz/nincal/helpers/calendar_helper.py
+++ nalinixyz/nincal/helpers/calendar_helper.py
@@ -6,9 +6,9 @@ def create_month(today):
     return calendar.Calendar().monthdatescalendar(today.year, today.month)
 
 
-def get_times(request, day=None):
+def get_times(request):
     return {
-        'day_iso' : datetime.datetime(*map(int, (day or request.GET.get('day') or request.GET.get('override')).split('-'))).date().isoformat(),
+        'day_iso' : datetime.datetime(*map(int, (request.GET.get('day') or request.GET.get('override')).split('-'))).date().isoformat(),
         'now_in_india' : datetime.datetime.now(timezone('Asia/Calcutta')),
         'now_in_america' : datetime.datetime.now(timezone('America/Los_Angeles')),
         'now_iso' : datetime.datetime.now(timezone('Asia/Calcutta')).date().isoformat()
